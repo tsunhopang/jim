b7e58acd21ce26e04e0464ac551a70f48809df39
Thomas Ng <thomas.ng@nikhef.nl>
2026-05-19 10:19:21 +0200

    fix: remove unnecessary variable M_c_min in CombinePrior for clarity

--- examples/GW170817_multiband.py
+++ examples/GW170817_multiband.py
@@ -65,11 +65,9 @@ waveform = IMRPhenomXAS_NRTidalv3(f_ref=20)
 
 # --- Prior ---
 
-M_c_min = 1.18
-
 prior = CombinePrior(
     [
-        UniformPrior(M_c_min, 1.21, parameter_names=["M_c"]),
+        UniformPrior(1.18, 1.21, parameter_names=["M_c"]),
         UniformPrior(0.125, 1.0, parameter_names=["q"]),
         UniformPrior(-0.05, 0.05, parameter_names=["s1_z"]),
         UniformPrior(-0.05, 0.05, parameter_names=["s2_z"]),
